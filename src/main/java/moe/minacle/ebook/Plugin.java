a374cd7f32e132f14dc2ab6f39d84cedccf679a8
Mina Her <minacle@live.com>
2023-11-04 14:03:24 +0900

    Adjust cost + Make enchantable to Enchanted Book item

--- src/main/java/moe/minacle/ebook/Plugin.java
+++ src/main/java/moe/minacle/ebook/Plugin.java
@@ -1,6 +1,5 @@
 package moe.minacle.ebook;
 
-import io.papermc.paper.enchantments.EnchantmentRarity;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -18,42 +17,86 @@ import java.util.Map;
 
 public final class Plugin extends JavaPlugin implements Listener {
 
+    /**
+     * Calculates the cost of an enchantment based on its rarity and level.
+     *
+     * @param enchantment
+     *  the enchantment to calculate the cost for
+     *
+     * @param level
+     *  the level of the enchantment
+     *
+     * @return
+     *  the cost of the enchantment, or -1 if the rarity is invalid
+     */
+    private static int getEnchantmentCost(Enchantment enchantment, int level) {
+        switch (enchantment.getRarity()) {
+        case COMMON:
+        case UNCOMMON:
+            return level;
+        case RARE:
+            return level * 2;
+        case VERY_RARE:
+            return level * 4;
+        default:
+            return -1;
+        }
+    }
+
     // MARK: JavaPlugin
 
     @Override
     public void onEnable() {
         super.onEnable();
-        this.getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     // MARK: Listener
 
     @EventHandler
     void onPrepareAnvil(PrepareAnvilEvent event) {
-        final AnvilInventory anvilInventory;
-        final int anvilInventoryRepairCost;
+        final AnvilInventory anvilInventory = event.getInventory();
         final ItemStack firstItem;
         final ItemMeta firstItemMeta;
+        final Material firstItemType;
+        final Map<Enchantment, Integer> firstItemEnchantments;
+        final boolean isFirstItemEnchantmentsEmpty;
+        final int firstItemRepairCost;
         final ItemStack secondItem;
         final ItemMeta secondItemMeta;
         final Material secondItemType;
         final Map<Enchantment, Integer> secondItemEnchantments;
         final int secondItemRepairCost;
-        ItemStack result;
+        final ItemStack anvilInventoryResult;
+        final Material anvilInventoryResultType;
+        final ItemStack result;
         final ItemMeta resultMeta;
         int totalEnchantmentCost = 0;
-        if (!this.isEnabled())
-            return;
-        anvilInventory = event.getInventory();
         if (
             (firstItem = anvilInventory.getFirstItem()) == null ||
-            (secondItem = anvilInventory.getSecondItem()) == null ||
-            firstItem.getType() != Material.BOOK
+            (secondItem = anvilInventory.getSecondItem()) == null
+        )
+            return;
+        firstItemType = firstItem.getType();
+        if (
+            firstItemType != Material.BOOK &&
+            firstItemType != Material.ENCHANTED_BOOK
+        )
+            return;
+        if ((anvilInventoryResult = anvilInventory.getResult()) != null)
+            anvilInventoryResultType = anvilInventoryResult.getType();
+        else
+            anvilInventoryResultType = null;
+        secondItemType = secondItem.getType();
+        if (
+            firstItemType == Material.ENCHANTED_BOOK &&
+            secondItemType == Material.ENCHANTED_BOOK &&
+            anvilInventoryResultType == Material.ENCHANTED_BOOK
         )
             return;
         if (
-            (result = anvilInventory.getResult()) != null &&
-            result.getType() != Material.BOOK
+            anvilInventoryResult != null &&
+            anvilInventoryResultType != Material.BOOK
         )
             return;
         firstItemMeta = firstItem.getItemMeta();
@@ -69,47 +112,77 @@ public final class Plugin extends JavaPlugin implements Listener {
             event.setResult(null);
             return;
         }
-        anvilInventoryRepairCost = anvilInventory.getRepairCost();
+        if (firstItemType == Material.ENCHANTED_BOOK) {
+            firstItemEnchantments = ((EnchantmentStorageMeta)firstItemMeta).getStoredEnchants();
+            result = firstItem.clone();
+            resultMeta = result.getItemMeta();
+        }
+        else {
+            firstItemEnchantments = firstItem.getEnchantments();
+            result = new ItemStack(Material.ENCHANTED_BOOK);
+            if (firstItemMeta != null)
+                resultMeta = Bukkit.getItemFactory().asMetaFor(firstItemMeta, result);
+            else
+                resultMeta = result.getItemMeta();
+        }
+        if (resultMeta == null)
+            return;
+        isFirstItemEnchantmentsEmpty = firstItemEnchantments.isEmpty();
+        if (isFirstItemEnchantmentsEmpty) {
+            final EnchantmentStorageMeta resultEnchantmentStorageMeta = (EnchantmentStorageMeta)resultMeta;
+            for (final Map.Entry<Enchantment, Integer> entry : secondItemEnchantments.entrySet()) {
+                final Enchantment enchantment = entry.getKey();
+                final int enchantmentLevel = entry.getValue();
+                if (
+                    firstItemType != Material.BOOK ||
+                    secondItemType != Material.ENCHANTED_BOOK
+                ) {
+                    final int enchantmentCost = getEnchantmentCost(enchantment, enchantmentLevel);
+                    if (enchantmentCost < 0)
+                        return;
+                    totalEnchantmentCost += enchantmentCost;
+                }
+                resultEnchantmentStorageMeta.addStoredEnchant(enchantment, enchantmentLevel, true);
+            }
+        }
+        else {
+            final EnchantmentStorageMeta resultEnchantmentStorageMeta = (EnchantmentStorageMeta)resultMeta;
+            for (final Map.Entry<Enchantment, Integer> entry : secondItemEnchantments.entrySet()) {
+                final Enchantment enchantment = entry.getKey();
+                final int enchantmentLevel = entry.getValue();
+                if (enchantmentLevel > firstItemEnchantments.getOrDefault(enchantment, 0)) {
+                    final int enchantmentCost = getEnchantmentCost(enchantment, enchantmentLevel);
+                    if (enchantmentCost < 0)
+                        return;
+                    totalEnchantmentCost += enchantmentCost;
+                    resultEnchantmentStorageMeta.addStoredEnchant(enchantment, enchantmentLevel, true);
+                }
+            }
+        }
+        if (firstItemMeta instanceof final Repairable firstItemRepairable)
+            firstItemRepairCost = firstItemRepairable.getRepairCost();
+        else
+            firstItemRepairCost = 0;
         if (secondItemMeta instanceof final Repairable secondItemRepairable)
             secondItemRepairCost = secondItemRepairable.getRepairCost();
         else
-            secondItemRepairCost = anvilInventoryRepairCost;
-        result = new ItemStack(Material.ENCHANTED_BOOK);
-        if (firstItemMeta != null)
-            resultMeta = Bukkit.getItemFactory().asMetaFor(firstItemMeta, result);
-        else
-            resultMeta = result.getItemMeta();
-        if (resultMeta == null)
-            return;
-        secondItemType = secondItem.getType();
-        for (final Map.Entry<Enchantment, Integer> entry : secondItemEnchantments.entrySet()) {
-            final Enchantment enchantment = entry.getKey();
-            final int enchantmentLevel = entry.getValue();
-            final EnchantmentRarity enchantmentRarity = enchantment.getRarity();
-            if (secondItemType != Material.ENCHANTED_BOOK) {
-                int enchantmentCost;
-                switch (enchantmentRarity) {
-                case COMMON:
-                case UNCOMMON:
-                    enchantmentCost = 1;
-                    break;
-                case RARE:
-                    enchantmentCost = 2;
-                    break;
-                case VERY_RARE:
-                    enchantmentCost = 4;
-                    break;
-                default:
-                    return;
+            secondItemRepairCost = 0;
+        if (totalEnchantmentCost > 0) {
+            if (
+                firstItemType == Material.BOOK &&
+                isFirstItemEnchantmentsEmpty
+            ) {
+                if (secondItemRepairCost > 0) {
+                    totalEnchantmentCost += (secondItemRepairCost + 1) / 2 - 1;
+                    ((Repairable)resultMeta).setRepairCost(secondItemRepairCost);
                 }
-                enchantmentCost *= enchantmentLevel;
-                totalEnchantmentCost += enchantmentCost;
             }
-            ((EnchantmentStorageMeta)resultMeta).addStoredEnchant(enchantment, enchantmentLevel, true);
+            else {
+                totalEnchantmentCost += firstItemRepairCost + secondItemRepairCost;
+                ((Repairable)resultMeta).setRepairCost((Math.max(firstItemRepairCost, secondItemRepairCost) + 1) * 2 - 1);
+            }
         }
-        if (secondItemRepairCost > 0) {
-            if (secondItemType != Material.ENCHANTED_BOOK)
-                totalEnchantmentCost += (secondItemRepairCost + 1) / 2 - 1;
+        else {
             ((Repairable)resultMeta).setRepairCost(secondItemRepairCost);
         }
         result.setItemMeta(resultMeta);
